a1f497fb18d1c5417de3bc605473f95f37019c0d
Ranvir Singh <ranvirsingh.st@gmail.com>
2025-04-11 16:28:20 -0700

    jank solve for 422 error

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi import FastAPI, Request, Response, HTTPException, Body, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from kvs import KeyValueStore
+from typing import Optional
 
 class ValueModel(BaseModel):
     value: str
     
 
 app = FastAPI()
+
+# JANK Solution. Overriding the default validation error 422 with 400.
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": exc.errors()}
+    )
+
 
 store = KeyValueStore()
 
@@ -15,13 +27,15 @@ def read_root():
     return {"message": "Hello world! This is the key-value store API."}
 
 @app.put("/data/{key}")
-def put_value(key: str, body: Optional[ValueModel] = None, response: Response):
+def put_value(key: str, response: Response, body: Optional[dict] = Body(default=None)):
     if body is None:
-        raise HTTPException(status_code = 400, detail = "Request body missing!")
-    if not isinstance(body.value, str):
-        raise HTTPException(status_code = 400, detail = "Value must be a string.")
-        
-    status = store.put(key, body.value)
+        raise HTTPException(status_code=400, detail="Request body missing!")
+
+    value = body.get("value")
+    if not isinstance(value, str):
+        raise HTTPException(status_code=400, detail="Field 'value' must be a string.")
+
+    status = store.put(key, value)
     response.status_code = status
     return {"message": "Created" if status == 201 else "Updated"}
 
@@ -34,7 +48,7 @@ def get_value(key: str):
 
 @app.delete("/data/{key}")
 def delete_key(key: str):
-    if store.delete(key)
+    if store.delete(key):
         return {"message" : "Key deleted"}
     raise HTTPException(status_code = 404, detail = "Key not found")
 
